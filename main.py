dfc933d0335ceb815b0eb3049cd7172df13268fb
KellenPeterson10 <818575@mystma.org>
2022-10-05 09:43:38 -0500

    dingle

---
+++ main.py
@@ -0,0 +1,18 @@
+myList = ["King", "Emperor", "Chinstrap", "Gentoo", "Little", "African", "Southern Rockhopper", "Macaroni"]
+pedHab = []
+pedDiet = []
+pedAppear = []
+pedConservStat = []
+
+P = input("Welcome to penguin fact. What penguin do you want to learn about? Here are your options!")
+
+print(myList[int(P)])
+print(pedHab[P])
+
+
+
+
+
+
+
+
