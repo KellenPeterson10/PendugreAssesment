e6f8d3cdcdfbb1c27a98f90aa6acb73c0d84c7fe
KellenPeterson10 <818575@mystma.org>
2022-10-06 09:42:51 -0500

    2

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import os
+from os import *
+def clear():
+    os.system("cls")
+clear()
 myList = ["King", "Emperor", "Chinstrap", "Gentoo", "Little", "African", "Southern Rockhopper", "Macaroni"]
-pedHab = []
-pedDiet = []
-pedAppear = []
-pedConservStat = []
-
-P = input("Welcome to penguin fact. What penguin do you want to learn about? Here are your options!")
-
+pedHab = ["", "", "", "", "", "", "", ""]
+pedDiet = ["", "", "", "", "", "", "", ""]
+pedAppear = ["", "", "", "", "", "", "", ""]
+pedConservStat = ["", "", "", "", "", "", "", ""]
+P = input ("Welcome to penguin fact. What penguin do you want to learn about?\n\n\n0. King, 1. Emperor, 2. Chinstrap, 3. Gentoo, 4. Little, 5. African, 6. Southern-Rockhopper, 7. Macaroni\n\n\n")
 print(myList[int(P)])
 print(pedHab[P])
 
